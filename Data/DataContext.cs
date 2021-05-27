8d310eca2ab23adc85b08a2d4424e784052e8506
Ibsylong <luanbanguyen94@gmail.com>
2021-05-27 15:42:34 +0200

    Add project files.

---
+++ Data/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClothesForAll.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Color> Colors { get; set; }
+        public DbSet<Size> Sizes { get; set; }
+        public DbSet<Image> Images { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Type> Types { get; set; }
+        public DbSet<Category>  Categories { get; set; }
+    }
+}
---
+++ Data/Product.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClothesForAll.Data
+{
+    public class Color 
+    {
+        public int ColorId { get; set; }
+        public string ColorName { get; set; }
+    }
+    public class Size
+    {
+        public int SizeId { get; set; }
+        public string SizeName { get; set; }
+    }
+
+    public class Image
+    {
+        public int ImageId { get; set; }
+        public string ImageSource { get; set; }
+
+    }
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Color> Colors { get; set; }
+        public string Description { get; set; }
+        public List<Size> Sizes { get; set; }
+        public List<Image> Images { get; set; }
+        public int Price { get; set; }
+        public int TypeId { get; set; }
+        public int CategoryId { get; set; }
+    }
+
+    public class Type
+    {
+        public int TypeId { get; set; }
+        public string TypeName { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class Category 
+    {
+        public int CategoryId { get; set; }
+        public string Gender { get; set; }
+    }
+}
